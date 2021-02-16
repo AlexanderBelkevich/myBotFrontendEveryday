3f7505e768210064dcfe04d07d21545dc75cac04
Alexander <alexbelkevich2504@gmail.com>
2021-02-16 15:48:09 +0400

    fixed

--- bot.js
+++ bot.js
@@ -1,8 +1,9 @@
-require('dotenv').config()
-
+require('dotenv').config();
 const { Telegraf, Markup, reply_markup } = require('telegraf')
+const api = require('covid19-api');
+const COUNTRIES_LIST = require('./constants');
 
-const bot = new Telegraf(process.env.BOT_TOKEN)
+const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot.on('new_chat_members', (ctx) => {
     ctx.reply(`
@@ -16,36 +17,32 @@ bot.on('new_chat_members', (ctx) => {
 А еще ты можешь набрать /start для запуска полезного бота.`)
 })
 
+bot.start((ctx) =>
+  ctx.telegram.sendMessage(ctx.update.message.from.id,
+    `
+С помощью кнопок ниже ты можешь получить интересующий тебя контент.
+Не стесняйся по ним жмякать :)
+`,
+Markup.keyboard([
+    ['Все задачи', 'Все разборы задач'],
+]).resize()
+  )
+);
+
+bot.help((ctx) => ctx.reply(COUNTRIES_LIST));
+
 bot.on('text', (ctx) => {
-    console.log(ctx)
-    console.log(ctx.message)
-    console.log(ctx.message.text.toLowerCase().replaceAll(" ", ""))
-//     if (ctx.update.message.text.toLowerCase() === 'Саня,покажизадачи') {
-//         ctx.telegram.sendMessage(ctx.update.message.from.id,`
-// <a href="https://www.patreon.com/posts/zadacha-no-1-47152476"><b>Задача №1</b>. Верстка чата</a>
-// <a href="https://www.patreon.com/posts/zadacha-no-2-47571187"><b>Задача №2</b>. Калькулятор стоимости доставки (до 22.02.2021)</a>
-//         `, {parse_mode: 'HTML'})
-//     }
-//     if (ctx.update.message.text === 'Все разборы задач') {
-//         ctx.telegram.sendMessage(ctx.update.message.from.id,`
-// <a href="https://youtu.be/aNM02KvjLu0"><b>Разбор задачи №1</b> от 14.02.2021</a>
-//         `, {parse_mode: 'HTML', disable_web_page_preview: true})
-//     }
+    if (ctx.update.message.text === 'Все задачи') {
+        ctx.telegram.sendMessage(ctx.update.message.from.id,`
+<a href="https://www.patreon.com/posts/zadacha-no-1-47152476"><b>Задача №1</b>. Верстка чата</a>
+<a href="https://www.patreon.com/posts/zadacha-no-2-47571187"><b>Задача №2</b>. Калькулятор стоимости доставки (до 22.02.2021)</a>
+        `, {parse_mode: 'HTML'})
+    }
+    if (ctx.update.message.text === 'Все разборы задач') {
+        ctx.telegram.sendMessage(ctx.update.message.from.id,`
+<a href="https://youtu.be/aNM02KvjLu0"><b>Разбор задачи №1</b> от 14.02.2021</a>
+        `, {parse_mode: 'HTML', disable_web_page_preview: true})
+    }
 })
-
-bot.start((ctx) => {
-    ctx.reply(`
-Напиши:
-"Саня, покажи задачи" для получения списка задач.
-"Саня, давай разборы" для получения списка разборов на Youtube.
-
-Ответ от бота придет в личные сообщения.
-`
-// Markup.keyboard([
-//     ['Все задачи', 'Все разборы задач'],
-// ]).resize()
-)
-})
-
-bot.launch()
-console.log('запущено')
+bot.launch();
+console.log('Бот запущен');
