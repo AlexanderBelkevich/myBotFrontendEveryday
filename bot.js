c00030047900d453e1ae6d11dd79f05f189553d8
Alexander <alexbelkevich2504@gmail.com>
2021-02-16 12:03:45 +0400

    change start text

--- bot.js
+++ bot.js
@@ -16,7 +16,7 @@ bot.start((ctx) => ctx.reply(`
 bot.on('new_chat_members', (ctx) => {
     console.log(ctx.update.message.new_chat_member)
     ctx.reply(`
-Привет, ${ctx.update.message.new_chat_member.first_name} ${ctx.update.message.new_chat_member.last_name}!
+Привет, ${ctx.update.message.new_chat_member.username}!
 
 Наше маленькое сообщество очень радо тому, что ты
 с нами!
