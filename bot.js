b1079e35a0825bf7472d51104e54cf342ee842ce
Alexander <alexbelkevich2504@gmail.com>
2021-02-16 13:13:16 +0400

    change text

--- bot.js
+++ bot.js
@@ -24,20 +24,17 @@ bot.on('new_chat_members', (ctx) => {
 А еще ты можешь набрать /start для запуска полезного бота :)`)
 })
 
-// bot.help((ctx) => ctx.reply(COUNTRIES_LIST))
-
 bot.on('text', async (ctx) => {
     console.log(ctx.update.message.text)
     if (ctx.update.message.text === 'Все задачи') {
-        console.log('ну ок же')
         ctx.reply(`
 <a href="https://www.patreon.com/posts/zadacha-no-1-47152476"><b>Задача №1</b>. Верстка чата</a>
 <a href="https://www.patreon.com/posts/zadacha-no-2-47571187"><b>Задача №2</b>. Калькулятор стоимости доставки (до 22.02.2021)</a>
         `, {parse_mode: 'HTML'})
     }
     if (ctx.update.message.text === 'Все разборы задач') {
-        console.log('ну ок же')
         ctx.reply(`
+<a href="https://youtu.be/aNM02KvjLu0"><b>Разбор задачи №1</b> от 14.02.2021</a>
 <a href="https://youtu.be/aNM02KvjLu0"><b>Разбор задачи №1</b> от 14.02.2021</a>
         `, {parse_mode: 'HTML'})
     }
