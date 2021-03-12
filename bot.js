dbfea7189ddbec17a45394a89c0b598b1cdc2143
Alexander <alexbelkevich2504@gmail.com>
2021-03-12 12:55:11 +0400

    s

--- bot.js
+++ bot.js
@@ -33,12 +33,15 @@ bot.on('text', async (ctx) => {
 <a href="https://www.patreon.com/posts/zadacha-no-1-47152476"><b>Задача №1</b>. Верстка чата</a>
 <a href="https://www.patreon.com/posts/zadacha-no-2-47571187"><b>Задача №2</b>. Калькулятор стоимости доставки (до 22.02.2021)</a>
 <a href="https://www.patreon.com/posts/zadacha-no-3-47942592"><b>Задача №3</b>. Административная панель (до 6.03.2021)</a>
+<a href="https://www.patreon.com/posts/zadacha-no-4-s-48528041"><b>Задача №4</b>. Работа с Google Maps (до 18.03.2021)</a>
+<a href="https://www.patreon.com/posts/zadacha-5-page-48654184"><b>Задача №5</b>. Верстка Landing Page (до 22.03.2021)</a>
         `, {parse_mode: 'HTML'})
     }
     if (ctx.update.message.text === 'Все разборы задач') {
         ctx.telegram.sendMessage(ctx.update.message.from.id,`
 <a href="https://youtu.be/aNM02KvjLu0"><b>Разбор задачи №1</b> от 14.02.2021</a>
 <a href="https://youtu.be/jf7jTFiWo_c"><b>Разбор задачи №2</b> от 24.02.2021</a>
+<a href="https://www.youtube.com/watch?v=onTPRUmWF64"><b>Разбор задачи №3</b> от 11.03.2021</a>
         `, {parse_mode: 'HTML', disable_web_page_preview: true})
     }
 })
