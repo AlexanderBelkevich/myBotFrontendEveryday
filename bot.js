4feb260d153cce78ace6f8d134eb2971be9dcbc3
Alexander <alexbelkevich2504@gmail.com>
2021-04-27 14:24:09 +0400

    new

--- bot.js
+++ bot.js
@@ -54,6 +54,7 @@ bot.on('text', async (ctx) => {
 <a href="https://www.youtube.com/watch?v=F7tdY4JU1lQ"><b>Разбор задачи №7</b> от 7.04.2021</a>
 <a href="https://youtu.be/E9n-vFwsGJc"><b>Разбор задачи №8</b> от 21.04.2021</a>
 <a href="https://www.youtube.com/watch?v=z347ls11ifw"><b>Разбор задачи №9</b> от 19.04.2021</a>
+<a href="https://youtu.be/WaY66lgjHUA"><b>Разбор задачи №10</b> от 26.04.2021</a>
         `, {parse_mode: 'HTML', disable_web_page_preview: true})
     }
 })
